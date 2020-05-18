c5861b4fee228b5d53b15acf9714ae59dbe89818
In Kyung Lee <37860222+9ruddls3@users.noreply.github.com>
2020-05-19 03:11:58 +0900

    Update parms.py

--- parms.py
+++ parms.py
@@ -1,38 +1,3 @@
-# import os
-# import numpy as np
-# import sys
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.gridspec as gridspec
-# import random
-# import time
-# import copy
-# import math
-# import PIL
-# import pandas as pd
-#
-# import torch
-# import torch.nn as nn
-#
-# from torch.nn import CTCLoss
-# import torch.optim as optim
-# import torch.nn.functional as F
-# from torch.autograd import Variable
-# from torch.utils.data import DataLoader
-# from torch.utils.data.sampler import SubsetRandomSampler
-# import torch.optim.lr_scheduler as lrs
-#
-# import torchvision
-# import torchvision.utils as utils
-# import torchvision.datasets as dset
-# import torchvision.transforms as transforms
-# import torchvision.models as models
-#
-# from PIL import Image
-# from torch.utils.data import Dataset, DataLoader
-# from torch.autograd import Variable
-# from torchvision import datasets, transforms
-
 vocab=['-','.'," ",' ',"'",'!',',']+[chr(ord('a')+i) for i in range(26)]+[chr(ord('A')+i) for i in range(26)]+[chr(ord('0')+i) for i in range(10)]
 chrToindex={}
 indexTochr={}
@@ -42,10 +7,8 @@ for c in vocab:
     indexTochr[cnt]=c
     cnt+=1
 
-
 vocab=['-','.'," ",' ',"'",'!',',']+[chr(ord('a')+i) for i in range(26)]+[chr(ord('A')+i) for i in range(26)]+[chr(ord('0')+i) for i in range(10)]
 vocab_size=cnt # uppercase and lowercase English characters and digits(26+26+10=62)
-
 
 batch_size=16
 sequence_len=28
@@ -58,6 +21,3 @@ use_VGG_extractor=False
 learning_rate=(4e-3)*(0.8**0)
 num_train = 2000
 
-# print('Write your input ')
-# x = input()
-# print(x)
