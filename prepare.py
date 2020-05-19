518ff991439fd9c8768083d1e76eef994cd01fa4
In Kyung Lee <37860222+9ruddls3@users.noreply.github.com>
2020-05-19 22:36:23 +0900

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -20,30 +20,35 @@ def dir_(Dir):
 
 def Preparing(In, Out, file_name, num_train, batch_size, shuffle=False):
     AbsDir = os.path.abspath(In)
-    name_List = []
     file_List = []
 
     for x in os.listdir(AbsDir):
         file_List.append(x)
-        name_List.append(x[:-4])
 
-    name_List.sort()
     file_List.sort()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     train_Data = []
     file_name = file_name +'.pth'
+    
+
+    if len(file_List)//batch_size !=0:
+        num_dataset= (len(file_List)//batch_size)*batch_size
+        pass
+    else:
+        num_dataset= len(file_List)
+        pass
+    
 
     for x, y in enumerate(list(zip(file_List, name_List))):
         img_path = Path + str(y[0])
         img = PIL.Image.open(img_path).convert("RGB")
         transform = transforms.Compose(
-            [transforms.Scale((224, 224)), transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25))]
+            [transforms.Scale((224, 224)), transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25))]
         )
         A = transform(img).to(device)
         train_Data.append((A, y[1]))
-        if x == num_train - 1:
+        if x == num_dataset - 1:
             break
 
     return torch.save(torch.utils.data.DataLoader(train_Data, batch_size=batch_size, shuffle=shuffle), Out + file_name)
@@ -51,12 +56,12 @@ def Preparing(In, Out, file_name, num_train, batch_size, shuffle=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in_dir', type=str, required=True, help='path to folder which contains the images')
-    parser.add_argument('--out_dir', type=str, required=True, help='Torch_Dataloader output path')
+    parser.add_argument('--In_dir', type=str, required=True, help='path to folder which contains the images')
+    parser.add_argument('--Out_dir', type=str, required=True, help='Torch_Dataloader output path')
 
     args = parser.parse_args()
-    from_path = os.path.abspath(dir_(args.in_dir))
-    to_path = os.path.abspath(dir_(args.out_dir))
+    from_path = os.path.abspath(dir_(args.In_dir))
+    to_path = os.path.abspath(dir_(args.Out_dir))
 
     while True:
         print("Enter Dataloader file name")
@@ -65,7 +70,7 @@ if __name__ == '__main__':
         if os.path.isfile(dir):
             print("Please Enter other name (it existed already)", end='\n')
         else:
-            Preparing(dir, f_name, n_data, b_size, sfle)
+            Preparing(from_path. to_path, f_name, n_data, b_size, sfle)
             print('DataLoader Creation Complete!')
             sys.exit()
 
